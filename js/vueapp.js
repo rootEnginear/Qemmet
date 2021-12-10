cf80c6b4e0602382d6a24def8177a81630a26307
rootEnginear <suthep.chanchuphol@outlook.com>
2021-12-11 03:43:20 +0700

    feat: add debug data in vueapp

--- js/vueapp.js
+++ js/vueapp.js
@@ -4,7 +4,10 @@ const app = new Vue({
 	el: '#app',
 	data: {
 		raw_string: '2;;h1cx',
+		// raw_string:
+		// 	'2;;sdgtdg csdgctdg sx/x csxc/x rx()ry()rz() crx()cry()crz() u1()u2(,)u3(,,) cu1()cu2(,)cu3(,,) sw csw ccsw b xyz cxcycz h ch p() cp() st csct i m',
 		target_lang: 'qiskit03',
+		// target_lang: 'openqasm3',
 	},
 	computed: {
 		qemmet_info: function () {
