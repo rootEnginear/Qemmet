c5e3a1c228a58585ea4df64d3be668a13c3a16f4
rootEnginear <suthep.chanchuphol@outlook.com>
2021-12-19 00:02:07 +0700

    feat: add some tests

--- src/qemmet.test.ts
+++ src/qemmet.test.ts
@@ -1,19 +1,29 @@
-import { throws } from 'assert'
+import { deepEqual, equal, throws } from 'assert'
 
 import { parseQemmetString } from './qemmet'
 
 describe('Qemmet', function () {
 	describe('parseQemmetString', function () {
-		it('should parse valid string without error', function () {
+		it('should parse ";;x" without error', function () {
 			parseQemmetString(';;x')
 		})
-		it('should parse invalid string with error', function () {
-			throws(
-				function() {
-					parseQemmetString(';;')
-				},
-				new Error('`gates_string` not found. The required format is `quantum_register?;classical_register?;gates_string`')
-			)
+		it('should parse ";;" with error', function () {
+			throws(function () {
+				parseQemmetString(';;')
+			}, new Error(
+				'`gates_string` not found. The required format is `quantum_register?;classical_register?;gates_string`'
+			))
+		})
+
+		it('shoud parse ";;ccx" as same as ";;ccx012"', function () {
+			const parsed = parseQemmetString(';;ccx')
+			const expected = parseQemmetString(';;ccx123')
+			deepEqual(parsed, expected)
+		})
+		it('shoud parse ";;ccx3" as same as ";;ccx312"', function () {
+			const parsed = parseQemmetString(';;ccx3')
+			const expected = parseQemmetString(';;ccx312')
+			deepEqual(parsed, expected)
 		})
 	})
 })
