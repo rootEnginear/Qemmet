a7642bfa4a329e89f24b00b0cb2791a633d3630c
rootEnginear <suthep.chanchuphol@outlook.com>
2022-01-05 22:27:58 +0700

    feat: support new measure structure

--- src/translators/qemmet.ts
+++ src/translators/qemmet.ts
@@ -2,10 +2,13 @@ import { QemmetParserOutput, QemmetGateInfo } from '../types'
 
 const getGateString = (gate_info: QemmetGateInfo[]): string => {
 	const processed_qemmet_string = gate_info
-		.map(({ control_count, gate_name, gate_params, gate_registers }) => {
+		.map(({ control_count, gate_name, gate_params, gate_registers, target_bit }) => {
 			const control_string = 'c'.repeat(control_count)
 			const param_string = gate_params ? `[${gate_params}]` : ''
-			return `${control_string}${gate_name}${param_string}${gate_registers.join(' ')}`
+			const measure_target = target_bit == null ? '' : `->${target_bit}`
+			return `${control_string}${gate_name}${param_string}${gate_registers.join(
+				' '
+			)}${measure_target}`
 		})
 		.join('')
 	return processed_qemmet_string
