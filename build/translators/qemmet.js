03077ba00271dc67ee55be15abb72c905b4c0082
rootEnginear <suthep.chanchuphol@outlook.com>
2022-01-12 02:11:12 +0700

    feat: support condition

--- build/translators/qemmet.js
+++ build/translators/qemmet.js
@@ -1,10 +1,26 @@
+const expandMeasurements = (gate_info) => {
+    return gate_info
+        .map(({ control_count, gate_name, gate_params, gate_registers, target_bit, condition }) => gate_name === 'm'
+        ? gate_registers.map((reg, index) => ({
+            control_count,
+            gate_name,
+            gate_params,
+            gate_registers: [reg],
+            target_bit: [target_bit?.[index] ?? reg],
+            condition,
+        }))
+        : { control_count, gate_name, gate_params, gate_registers, target_bit, condition })
+        .flat();
+};
 const getGateString = (gate_info) => {
-    const processed_qemmet_string = gate_info
-        .map(({ control_count, gate_name, gate_params, gate_registers, target_bit }) => {
+    const expanded_gate_info = expandMeasurements(gate_info);
+    const processed_qemmet_string = expanded_gate_info
+        .map(({ control_count, gate_name, gate_params, gate_registers, target_bit, condition }) => {
         const control_string = 'c'.repeat(control_count);
         const param_string = gate_params ? `[${gate_params}]` : '';
-        const measure_target = target_bit == null ? '' : `->${target_bit}`;
-        return `${control_string}${gate_name}${param_string}${gate_registers.join(' ')}${measure_target}`;
+        const measure_target = target_bit.length ? `->${target_bit}` : '';
+        const condition_string = condition ? `?${condition[0]}=${condition[1]}` : '';
+        return `${control_string}${gate_name}${param_string}${gate_registers.join(' ')}${measure_target}${condition_string}`;
     })
         .join('');
     return processed_qemmet_string;
